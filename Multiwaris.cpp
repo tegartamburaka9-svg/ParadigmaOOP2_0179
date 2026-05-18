9930eacfcfc2a53dfffded372843e71d5945a954
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-18 15:12:53 +0700

    membuat calss pelajar

--- Multiwaris.cpp
+++ Multiwaris.cpp
@@ -20,3 +20,12 @@ public:
     }
 };
 
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
