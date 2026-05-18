f771a8e696259384a70e734a45b33cfd8dbbf27b
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-18 15:12:23 +0700

    membuat class pekerja

--- Multiwaris.cpp
+++ Multiwaris.cpp
@@ -11,3 +11,12 @@ public:
     }
 };
 
+class pekerja : virtual public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
