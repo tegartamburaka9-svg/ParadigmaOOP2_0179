3abd6a1d22008cb292e34f3d859a56033932b172
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-18 14:57:05 +0700

    membuat public abtraksiklas

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -5,3 +5,18 @@ class AbstraksiKlas {
 private:
     string x, y;
 
+public:
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+
+    //menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
